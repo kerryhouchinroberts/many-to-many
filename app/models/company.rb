c6787ed5d3373199560b43163cf825af57ce8409
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-25 15:09:45 -0700

    Create relationship between company, user and company review, and add company reviewer name to the reviews index

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ActiveRecord::Base
-
+  has_many :company_reviews
+  has_many :users, through: :company_reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :product_reviews
   has_many :products, through: :product_reviews
+
+  has_many :company_reviews
+  has_many :companies, through: :company_reviews
 end
